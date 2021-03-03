b786abcc3cce48338262d2fe4b2ade4ed2126bcb
zero <lidm@ziroom.com>
2021-03-03 23:50:12 +0800

    排序算法学习

---
+++ forkjoin/src/main/java/fit/ome/ForkJoinUtils.java
@@ -0,0 +1,13 @@
+package fit.ome;
+
+import java.util.concurrent.ForkJoinPool;
+
+/**
+ * @version 0.0.1-SNAPSHOT
+ * @auther Zero
+ * @date 2021/1/10
+ **/
+public class ForkJoinUtils {
+    public static ForkJoinPool forkJoinPool = ForkJoinPool.commonPool();
+
+}
