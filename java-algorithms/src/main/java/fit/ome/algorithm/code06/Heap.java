380e3e160332e95b8a95b2b881a50644ad73096d
zero <lidm@ziroom.com>
2021-04-04 23:06:21 +0800

    heap

--- java-algorithms/src/main/java/fit/ome/algorithm/code06/Heap.java
+++ java-algorithms/src/main/java/fit/ome/algorithm/code06/Heap.java
@@ -2,13 +2,13 @@ package fit.ome.algorithm.code06;
 
 /**
  * 底层是 AVL(Adelson-Velsky-Landis Tree) 完全二叉树 实现的
- *
+ * <p>
  * 在数组中，构建avl树，给定一个节点i，
  * 1. 做孩子位置 2*i+1
  * 2. 右孩子位置 2*i+2
  * 3. 父节点 (i+1)/2 -1
  * <br>
- *     默认实现，构建大根堆
+ * 默认实现，构建大根堆
  */
 public class Heap {
     /**
@@ -18,28 +18,22 @@ public class Heap {
      * @param index 新插入元素的位置
      */
     public void heapInsert(int[] arr, int index) {
-        int p=index;
-        int parent=(p+1)/2-1;
+
         // 元素需要和自己的父级元素进行大小比较
-        while (parent>0){
-            if(arr[p]>arr[parent]){
-                swap(arr,index,parent);
-                p=parent;
-                parent=(p+1)/2-1;
-            }else {
-                // 不大于自己的父节点，不需要在进行insert判断
-                break;
-            }
+        while (arr[index] > arr[(index - 1) / 2]) {
+            swap(arr, index, (index - 1) / 2);
+            index = (index - 1) / 2;
         }
 
     }
-    private void swap(int[] arr,int i,int j){
-        if(i==j){
+
+    private void swap(int[] arr, int i, int j) {
+        if (i == j) {
             return;
         }
-        arr[i]=arr[i]^arr[j];
-        arr[j]=arr[i]^arr[j];
-        arr[i]=arr[i]^arr[j];
+        arr[i] = arr[i] ^ arr[j];
+        arr[j] = arr[i] ^ arr[j];
+        arr[i] = arr[i] ^ arr[j];
 
     }
 
