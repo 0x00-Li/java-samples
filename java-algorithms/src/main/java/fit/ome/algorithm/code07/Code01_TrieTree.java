25c5ec599cda4ac6815020e348a395e5b3ee75ee
zero <lidm@ziroom.com>
2021-04-14 11:05:49 +0800

    char trie

--- java-algorithms/src/main/java/fit/ome/algorithm/code07/Code01_TrieTree.java
+++ java-algorithms/src/main/java/fit/ome/algorithm/code07/Code01_TrieTree.java
@@ -1,5 +1,8 @@
 package fit.ome.algorithm.code07;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Trie Tree
  * <p>
@@ -71,6 +74,135 @@ public class Code01_TrieTree {
          */
         public int searchPrefix(String word) {
             return 0;
+        }
+
+    }
+    // --------------------
+    // 第二种实现方式
+    // 和 1 的差别是支持任意字符
+
+    public static class CharNode {
+        /**
+         * 经过当前节点的字符串数
+         */
+        int pass;
+        /**
+         * 以这个字符结尾的字符串的数量
+         */
+        int end;
+        Map<Integer, CharNode> nextsMap;
+
+        public CharNode() {
+            pass = 0;
+            end = 0;
+            nextsMap = new HashMap<>();
+        }
+    }
+
+    public static class CharTrieTree {
+        CharNode root;
+
+        public CharTrieTree() {
+            root = new CharNode();
+        }
+
+        /**
+         * 增加字符串
+         *
+         * @param word
+         */
+        public void insert(String word) {
+            if (word == null) {
+                return;
+
+            }
+            char[] s = word.toCharArray();
+            CharNode node = root;
+            node.pass++;
+            for (int i = 0; i < s.length; i++) {
+                CharNode curNode = node.nextsMap.get(s[i]);
+                if (curNode == null) {
+                    curNode = new CharNode();
+                    node.nextsMap.put((int) s[i], curNode);
+                }
+                curNode.pass++;
+
+            }
+            node.end++;
+
+        }
+
+        /**
+         * 删除字符串
+         *
+         * @param word
+         */
+        public void delete(String word) {
+            if (search(word) == 0) {
+                return;
+            }
+            char[] s = word.toCharArray();
+            CharNode node = root;
+            node.pass--;
+            for (int i = 0; i < s.length; i++) {
+                if (--node.nextsMap.get((int) s[i]).pass == 0) {
+                    node.nextsMap.remove((int) s[i]);
+                    return;
+                }
+                node = node.nextsMap.get((int) s[i]);
+            }
+            node.end--;
+        }
+
+        /**
+         * 搜索word出现过几次
+         * <p>
+         * <p>
+         * 需要一直匹配到字符串的最后一个字符，判断结尾字符出现的 次数
+         *
+         * @param word
+         * @return
+         */
+        public int search(String word) {
+            if (word == null) {
+                return 0;
+            }
+            char[] s = word.toCharArray();
+            CharNode node = root;
+            int index = 0;
+            for (int i = 0; i < s.length; i++) {
+                index = s[i];
+                node = root.nextsMap.get(index);
+                if (node == null) {
+                    return 0;
+                }
+            }
+            return node.end;
+        }
+
+        /**
+         * 搜索相同前缀的单词数
+         *
+         * @param word
+         * @return
+         */
+        public int searchPrefix(String word) {
+            if (word == null) {
+                return 0;
+            }
+            char[] s = word.toCharArray();
+            CharNode node = root;
+            node.pass--;
+            int index = 0;
+            for (int i = 0; i < s.length; i++) {
+                index = s[i];
+                node = node.nextsMap.get(index);
+                if (node == null) {
+                    return 0;
+                }
+
+            }
+            return node.pass;
         }
 
     }
