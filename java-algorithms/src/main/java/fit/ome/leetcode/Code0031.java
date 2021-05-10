f4569754796f7b8084efbab1fdfc08efb65b4ff8
zero <lidm@ziroom.com>
2021-05-11 01:32:07 +0800

    leetcode

--- java-algorithms/src/main/java/fit/ome/leetcode/Code0031.java
+++ java-algorithms/src/main/java/fit/ome/leetcode/Code0031.java
@@ -16,60 +16,42 @@ public class Code0031 {
             swap(nums,0,1);
             return;
         }
-        for (int i = nums.length - 1; i >= 0; i--) {
-            if (i - 1 > 0 && nums[i] > nums[i - 1]) {
-                swap(nums, i, i - 1);
-                return;
-            }
+
+        int lessIndex = nums.length - 2;
+        while (lessIndex >= 0 && nums[lessIndex] >= nums[lessIndex + 1]) {
+            lessIndex--;
         }
-
-        if (nums[0] > nums[1]) {
-            for (int i = 0, j = nums.length - 1; j > i && i < nums.length; i++, j--) {
-                swap(nums, i, j);
+        if (lessIndex >= 0) {
+            // 非全局逆序
+            int moreIndex = nums.length - 1;
+            while (moreIndex >= 0 && nums[moreIndex] <= nums[lessIndex]) {
+                moreIndex--;
             }
-            return;
-        }
-
-
-        int mid = nums[0];
-        boolean hasZero = false;
-        for (int i = 1, j = nums.length - 1; j > i && i < nums.length; i++, j--) {
-
-
-            if (!hasZero && nums[j] > mid) {
-//                nums[0] = nums[j];
-//                nums[j] = mid;
-                swap(nums,0,j);
-                swap(nums, i, j);
-                hasZero = true;
-            } else if (nums[j - 1] <= mid && nums[i + 1] > mid) {
-                swap(nums, i, j);
-            } else if (!hasZero && nums[j - 1] > mid) {
-                // 留住j位置数字
-               swap(nums,0,j-1);
-                swap(nums, i, j);
-                hasZero = true;
-            } else if (!hasZero && nums[i + 1] < mid) {
-//                留住i位置数字
-//                nums[0] = nums[i];
-//                nums[i] = mid;
-                swap(nums,0,i);
-                swap(nums, i, j);
-                hasZero = true;
-            }
+            swap(nums, moreIndex, lessIndex);
 
         }
+        reverse(nums, lessIndex + 1);
+
 
     }
 
+    public void reverse(int[] nums, int start) {
+        for (int i = start, j = nums.length - 1; j > i; i++, j--) {
+            swap(nums, i, j);
+        }
+    }
+
     public void swap(int[] nums, int i, int j) {
+        if(i==j){
+            return;
+        }
         nums[i] = nums[i] ^ nums[j];
         nums[j] = nums[i] ^ nums[j];
         nums[i] = nums[i] ^ nums[j];
     }
 
     public static void main(String[] args) {
-        int[] nums = {3, 5, 4, 3, 2};
+        int[] nums = {1,1};
         new Code0031().nextPermutation(nums);
         printArr(nums);
     }
