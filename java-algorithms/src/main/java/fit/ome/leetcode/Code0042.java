1c6e8c75595305736e1b370086a33cbe75d8e3e3
zero <lidm@ziroom.com>
2021-05-12 10:07:14 +0800

    code 32

---
+++ java-algorithms/src/main/java/fit/ome/leetcode/Code0042.java
@@ -0,0 +1,18 @@
+package fit.ome.leetcode;
+
+/**
+ * 接雨水问题
+ * https://leetcode-cn.com/problems/trapping-rain-water/
+ * <p>
+ *
+ * 使用单调栈实现
+ *
+ * 通过求最近最大边界进行，求解雨水面积
+ *
+ */
+public class Code0042 {
+    public int trap(int[] height) {
+        return 0;
+    }
+
+}
