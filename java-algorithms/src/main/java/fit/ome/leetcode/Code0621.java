e543ed9d2d6b073041fe5d7c8df273dad44727e5
zero <lidm@ziroom.com>
2021-05-13 18:16:13 +0800

    leetcode

---
+++ java-algorithms/src/main/java/fit/ome/leetcode/Code0621.java
@@ -0,0 +1,18 @@
+package fit.ome.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 任务调度器
+ * https://leetcode-cn.com/problems/task-scheduler/
+ */
+public class Code0621 {
+    public int leastInterval(char[] tasks, int n) {
+        if (tasks == null || tasks.length == 0) {
+            return 0;
+        }
+        Map<Character, Integer> taskCount = new HashMap<>();
+        return 0;
+    }
+}
