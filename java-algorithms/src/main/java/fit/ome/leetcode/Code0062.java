56aabc8688ca42dd3554e3f44d7ac243c6a67cd6
zero <lidm@ziroom.com>
2021-05-17 18:52:06 +0800

    leetcode

---
+++ java-algorithms/src/main/java/fit/ome/leetcode/Code0062.java
@@ -0,0 +1,12 @@
+package fit.ome.leetcode;
+
+/**
+ * 不同路径
+ * <p>
+ * https://leetcode-cn.com/problems/unique-paths/
+ */
+public class Code0062 {
+    public int uniquePaths(int m, int n) {
+        return 0;
+    }
+}
