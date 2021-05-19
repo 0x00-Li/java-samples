175a1d8bd9485411bcfae4232c082332768e5dea
zero <lidm@ziroom.com>
2021-05-19 16:09:28 +0800

    algorithm

---
+++ java-algorithms/src/main/java/fit/ome/algorithm/code19/Code04_LongestCommonSubsequence.java
@@ -0,0 +1,17 @@
+package fit.ome.algorithm.code19;
+
+/**
+ * 最长公共子序列
+ * https://leetcode.com/problems/longest-common-subsequence/
+ * <p>
+ * 给定两个字符串str1和str2，
+ * 返回这两个字符串的最长公共子序列长度
+ * <p>
+ * 比如 ： str1 = “a12b3c456d”,str2 = “1ef23ghi4j56k”
+ * 最长公共子序列是“123456”，所以返回长度6
+ */
+public class Code04_LongestCommonSubsequence {
+    public static int longestCommonSubsequence1(String s1, String s2) {
+        return 0;
+    }
+}
