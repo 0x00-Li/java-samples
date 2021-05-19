ccbfe9bd57d34de4615555f9cd1d7a7bab97eee0
zero <lidm@ziroom.com>
2021-05-19 10:59:37 +0800

    algorithm

---
+++ java-algorithms/src/main/java/fit/ome/leetcode/Code0072.java
@@ -0,0 +1,16 @@
+package fit.ome.leetcode;
+
+/**
+ * 编辑距离
+ *
+ * https://leetcode-cn.com/problems/edit-distance/
+ *
+ * dfs
+ *
+ * 和动态规划
+ */
+public class Code0072 {
+    public int minDistance(String word1, String word2) {
+return 0;
+    }
+}
---
+++ java-algorithms/src/main/java/fit/ome/leetcode/Code1143.java
@@ -0,0 +1,18 @@
+package fit.ome.leetcode;
+
+/**
+ * 最长公共子序列
+ *
+ *
+ * https://leetcode-cn.com/problems/longest-common-subsequence/
+ *
+ *
+ * 动态规划
+ *
+ */
+public class Code1143 {
+    public int longestCommonSubsequence(String text1, String text2) {
+
+        return 0;
+    }
+}
